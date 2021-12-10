7af7d19a6536cd1efced9b0361c032dbdbde7ade
KSFlynn007 <kelseyflynnn@gmail.com>
2021-12-09 17:50:09 -0700

    Update NewsFeed.js
    added newsfeed component

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const NewsFeed = () => {
+
+    const [articles, setArticles] = useState(null);
+
+    useEffect(() => {
+        var options = {
+        method: 'GET',
+        url: 'https://crypto-news-live.p.rapidapi.com/news',
+        headers: {
+            'x-rapidapi-host': 'crypto-news-live.p.rapidapi.com',
+            'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY
+        }
+        };
+
+        axios.request(options).then((response) => {
+            // console.log(response.data);
+            setArticles(response.data)
+        }).catch((error) => {
+            console.error(error);
+        });
+        // no dependencies, runs only once on mount
+    }, [])
+
+    // ? is short form notation for ternary operator = if ? true : false
+    // checks if existing first before enacting slice
+    const first7 = articles?.slice(0, 7)
+
     return (
         <div className="news-feed">
-            Newsfeed
+            <h2>NewsFeed</h2>
+            {first7?.map((article, _index) => 
+            <div>
+                <p key={_index}>
+                    <a 
+                        href={article.url} 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        className="news-link"                    
+                    >
+                        {article.title}
+                    </a>
+                </p>
+            </div>
+            )}
         </div>
     )
 }
